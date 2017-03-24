c1e4a0030183253238a78ded579ddab995f7d607
Juanjo Rodriguez <jordan@devcamp.com>
2017-03-24 12:58:48 +0000

    Implemented image defaults for posrtfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,12 +15,10 @@ class PortfoliosController < ApplicationController
    @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio_itemdd.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
